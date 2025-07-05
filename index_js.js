e2a969c59b3e5707ebe9e9f5889491c7fcf8107e
Love <love-mary.victor@outlook.com>
2025-07-05 14:50:09 -0400

    designing first page of form

--- index_js.js
+++ index_js.js
@@ -4,11 +4,11 @@ let clickButtons = Array.from(document.getElementsByClassName("click-button"));
 const allClickable = cardBundle.concat(clickButtons);
 
 allClickable.forEach(element => {
-  element.addEventListener("click",clickToStartForm) 
+  element.addEventListener("click", clickToStartForm)
 });
 
 
-function clickToStartForm(){
-    window.location.href = "./first_page.html";
-    
+function clickToStartForm() {
+  window.location.href = "./first_page.html";
+
 }
