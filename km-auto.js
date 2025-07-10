33dd411aa0f42a8448510c91979cfcf5b2e98c47
Love <love-mary.victor@outlook.com>
2025-07-09 21:17:49 -0400

    fixing km 50000 accepting them at 50k

--- km-auto.js
+++ km-auto.js
@@ -11,12 +11,12 @@ function isValueEntered(value) {
 
 function isKmValidFinal(value) {
     const numValue = parseFloat(value);
-    return !isNaN(numValue) && numValue >= 0 && numValue < 50000;
+    return !isNaN(numValue) && numValue >= 0 && numValue <= 50000;
 }
 
 function handleInputAndButtonState() {
     let writtenValue = kmAutoInput.value;
-    
+
     localStorage.setItem("kmAnnuel", writtenValue);
 
     if (isValueEntered(writtenValue)) {
@@ -29,11 +29,11 @@ function handleInputAndButtonState() {
 
 kmAutoInput.addEventListener("input", handleInputAndButtonState);
 
-boutonAvancer.addEventListener("click", function(event) {
-    event.preventDefault(); 
-    
+boutonAvancer.addEventListener("click", function (event) {
+    event.preventDefault();
+
     let writtenValue = kmAutoInput.value;
-    
+
     if (isKmValidFinal(writtenValue)) {
         window.location.href = this.href;
     } else {
@@ -41,11 +41,11 @@ boutonAvancer.addEventListener("click", function(event) {
     }
 });
 
-kmAutoInput.addEventListener("keydown", function(event) {
+kmAutoInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         let writtenValue = this.value;
         localStorage.setItem("kmAnnuel", writtenValue);
-        
+
         if (isKmValidFinal(writtenValue)) {
             window.location.href = "camera-recul.html";
         } else {
